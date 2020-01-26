7305c48327b9b90fb08b6511c236f2e9c8185e90
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-01-26 13:49:06 -0300

    ok

--- resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
+++ resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
@@ -5,12 +5,12 @@
 
 {{--*/ $ImagenPortada         = url() . '/imagenes/PaginasPersonalizadas/fabian-delpino-abogado-especialista-en-desalojos-en-uruguay.jpg'/*--}}
 {{--*/ $ImagenParaTaG         = url() . '/imagenes/PaginasPersonalizadas/fabian-delpino-abogado-experto-en-desalojo-uruguay-montevideo.jpg'/*--}}
-{{--*/ $Titulo                = 'Páginas web para PyMes' /*--}} 
+{{--*/ $Titulo                = 'Abogado experto en desalojos en Uruguay | Fabian Delpino' /*--}} 
 {{--*/ $DescriptionEtiqueta   = 'Una página web es la vidriera perfecta para una PyMe 
 Estoy convencido de que tener un sitio web es algo muy importante, seas un emprendedor que recién comienza, seas una empresa pequeña o una gran empresa, o si tu mercado es global o local: contar con una página web te permitirá dar a conocer tus beneficios de forma masiva y de forma muy simple. ¿Tu objetivo es aumentar las ventas de tu negocio? ¿Conseguir nuevos clientes? ¿Mejorar la imagen de tu empresa?  todo esto lo puedes lograr con una página web.
  ' /*--}}
 {{--*/ $PalabrasClaves        = '' /*--}}
-{{--*/ $UrlDeLaPagina         = route('get_pagina_de_web_para_pymes') /*--}}
+{{--*/ $UrlDeLaPagina         = route('get_home') /*--}}
 
 @section('og-tags')
  <meta property="og:type"               content="website" />
