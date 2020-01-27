bf9d838a8ddb2f9d08bab7c16421cc6fd78c8c49
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-01-26 21:14:50 -0300

    sd

--- resources/views/paginas/home/vue/contacto-component.blade.php
+++ resources/views/paginas/home/vue/contacto-component.blade.php
@@ -4,9 +4,13 @@ props:['empresa']
 ,  
 data:function(){
     return {
-      name:'',
-      email:'',
-      mensaje:'',
+     data_enviar:{ 
+                   name:'',
+                   email:'',
+                   mensaje:'',
+                   inquilino_ocupante:'',
+      
+      },
       se_envio:false,
       mensaje_se_envio:'',
       errores:false
@@ -16,11 +20,7 @@ data:function(){
 methods:{
 
 enviarMensaje:function(){
-  var data = {
-                name:this.name,
-                email:this.email,
-                mensaje:this.mensaje
-             };
+  var data = this.data_enviar;
 
   var url  = '/post_contacto_form';
   var vue  = this;
@@ -56,26 +56,43 @@ template:'
 <div v-if="!se_envio" class="container">
   <div class="row">
     <div class="col-12 mb-5 position-relative">
-      <h2 class="section-title text-center text-white mb-5">Contacto</h2>
+      <h2 class="section-title text-center text-white mb-2">¿Quieres qué te ayude?</h2>
+      <p class="text-center text-white mb-5">Si quieres recibir mi ayuda me tienes que dar algunos datos para que te pueda decir cuánto va a demorar el proceso y cuanto te costará todo el trámite.</p>
     </div>
   </div>
   <div action="#" class="form">
+
+    <div class="row mb-4">
+      <div class="contacto-titulo-pregunta">¿Cuál es tu caso?</div>
+      <div class="contacto-contiene-opciones">
+        <div class="contacto-contiene-opcion-individual">
+            <div class="contacto-contiene-opcion-individual-titulo">Problemas con un inquilino</div>  
+            <input type="radio" name="" value="inquilino" v-model="data_enviar.inquilino_ocupante">        
+        </div>
+        <div class="contacto-contiene-opcion-individual">
+            <div class="contacto-contiene-opcion-individual-titulo">Problemas con un inquilino</div>  
+            <input type="radio" name="" value="ocupante" v-model="data_enviar.inquilino_ocupante">        
+        </div>
+      </div>
+      
+
+    </div>
     <div class="row mb-4">
       
       <div class="form-group col-12">
-        <input v-model="name" type="text" class="form-control" placeholder="Nombre">
+        <input v-model="data_enviar.name" type="text" class="form-control" placeholder="Nombre">
       </div>
     </div>
 
     <div class="row mb-4">
       <div class="form-group col-12">
-        <input v-model="email" type="email" class="form-control" placeholder="Email ">
+        <input v-model="data_enviar.email" type="email" class="form-control" placeholder="Email ">
       </div>
     </div>
    
     <div class="row mb-4">
       <div class="form-group col-12">
-        <input type="text" v-model="mensaje" cols="30" rows="10" class="form-control" placeholder="Escribe">
+        <input type="text" v-model="data_enviar.mensaje" cols="30" rows="10" class="form-control" placeholder="Escribe">
       </div>
     </div>
 
