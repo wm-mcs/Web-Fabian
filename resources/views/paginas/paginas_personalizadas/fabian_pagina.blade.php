688b7a1cea15faf4476d111aca4022da0f880468
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-03-29 17:21:39 -0300

    sd

--- resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
+++ resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
@@ -140,14 +140,14 @@
     
   </ol>
   <div class="carousel-inner">
-    <div class="carousel-item active Slider-contenedor" ">
+    <div class="carousel-item active Slider-contenedor">
       <img class="post-img-slider-size Slider-img"  src="{{$ImagenPortada}}" alt="Fabian Delpino"> 
       <img class="logo-float-easy-socio-portada" :src="empresa.img_logo_horizontal_blanco" alt="Estudio Delpino y Sanchez">
       <div class="Slider-contenido-de-textos-y-botones">
        <div class="Slider-wraper-contenido-de-textos-y-botones">
          <h2>Hola</h2>
          <p>Hola estoy probando texto</p>
-         
+
        </div>
         
       </div>
