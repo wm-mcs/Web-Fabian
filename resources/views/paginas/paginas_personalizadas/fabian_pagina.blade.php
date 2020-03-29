cc402c106ad48c903f11191800b7671920f36a0f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-03-29 16:47:35 -0300

    sd

--- resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
+++ resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
@@ -128,13 +128,15 @@
 
 @section('contenido')
 
+<div class="get_width_100">
+  
 
 
   <div  class="carousel slide home_slider" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -148,6 +150,8 @@
     
   </div>
  
+</div>
+
 </div>
    
 
