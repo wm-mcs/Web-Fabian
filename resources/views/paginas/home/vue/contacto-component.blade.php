4e12ca0a27072dc800d4c28c2ae158a528ef64eb
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-03-30 21:38:37 -0300

    sd

--- resources/views/paginas/home/vue/contacto-component.blade.php
+++ resources/views/paginas/home/vue/contacto-component.blade.php
@@ -159,18 +159,20 @@ template:'
 
 
 
-    <div v-if="mostrar_campos" class="row mb-4">
+    <div v-if="mostrar_campos" class="row mb-2">
       
-      <div class="form-group col-sm-4">
+      <div class="form-group col-xs-6  col-sm-4">
         <input v-model="data_enviar.name" type="text" class="form-control" placeholder="Escribe tu nombre">
       </div>
 
-      <div class="form-group col-sm-4">
-        <input v-model="data_enviar.email" type="email" class="form-control" placeholder="Escribe tu email ">
+      
+
+      <div class="form-group col-xs-6 col-sm-4">
+        <input v-model="data_enviar.celular"  type="text"  class="form-control" placeholder="Escribe tu celular">
       </div>
 
-      <div class="form-group col-sm-4">
-        <input v-model="data_enviar.celular"  type="text"  class="form-control" placeholder="Escribe tu celular">
+      <div class="form-group  col-sm-4">
+        <input v-model="data_enviar.email" type="email" class="form-control" placeholder="Escribe tu email ">
       </div>
     </div>
 
