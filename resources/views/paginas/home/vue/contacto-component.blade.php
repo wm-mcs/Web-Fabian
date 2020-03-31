b878a213a0f87a93efd3692f46b7f0516283f61e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-03-31 10:04:01 -0300

    sd

--- resources/views/paginas/home/vue/contacto-component.blade.php
+++ resources/views/paginas/home/vue/contacto-component.blade.php
@@ -127,7 +127,7 @@ computed:{
 },
 template:'
  <section class="section-contacto-class" id="contact-section">
- <img class="section-contacto-img-background" src="{{url()}}/imagenes/PaginasPersonalizadas/Delpino-website-typo-contact.jpg">
+ 
 
 <div class="contacto-absolut-wraper">
 <div class="contacto-contenido-wraper">
