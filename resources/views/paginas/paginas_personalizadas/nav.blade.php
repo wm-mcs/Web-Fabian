eb3d90318e4c61d2b58588d5a94f9c7ed79157fb
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-21 15:25:28 -0300

    df

---
+++ resources/views/paginas/paginas_personalizadas/nav.blade.php
@@ -0,0 +1,51 @@
+<header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner" id="header">
+
+      <div class="container-fluid"  style="padding: 0 30px; max-width:1650px;">
+        <div class="row align-items-center justify-content-between">
+          
+          <div class="">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu  mr-auto ">
+                <li>
+                    <a v-show="mostrar_logo_nav" href="{{url()}}" class="nav-link">
+                      <img v-if="mostrar_para_grande" style="height: 30px;" :src="empresa.img_logo_horizontal_blanco">
+                      <img v-else style="height: 40px;" :src="empresa.img_logo_horizontal_v2_blanco">
+                      
+                    </a>
+                </li>
+                
+                
+              </ul>
+            </nav>
+          </div>
+
+        
+
+          <div class="text-left">
+
+            <nav class="site-navigation position-relative" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">  
+                <li><a href="{{url()}}" class="nav-link">INICIO</a></li>             
+                <li><a href="#sobre-mi" class="nav-link">QUIÉNES SOMOS</a></li>
+                <li><a href="{{route('get_servicios')}}" class="nav-link">SERVICIOS</a></li> 
+                <li><a href="{{route('get_contacto')}}" class="nav-link">CONTACTO</a></li>
+                @if(!Auth::guest())
+                  <li><a href="{{route('get_datos_corporativos')}}" class="nav-link">ADMINISTRAR</a></li>
+                  <li><a href="{{route('logout')}}" class="nav-link">SALIR</a></li>
+                @endif
+              </ul>
+            </nav>
+
+
+            <div class="flex-row-center d-lg-none" style="position: relative; top: 3px;">
+              <a v-show="mostrar_logo_nav" href="{{url()}}" class="inicio-class-al-lado-de-boton">INICIO</a>
+              <a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+      
+    </header>
