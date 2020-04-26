5b1cce1f93696fd8114be320708754ffc2c81bed
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-26 19:13:03 -0300

    sd

--- resources/views/emails/solicitud_contacto.blade.php
+++ resources/views/emails/solicitud_contacto.blade.php
@@ -1,32 +1,34 @@
 <br>
 
-Solicitud de contacto por web:
-
+<span style="font-family: arial; font-size: 14px; padding-bottom:5px; font-weight: bold; ">
+ Solicitud de contacto por web:
+</span>
+<br>
 <br>
 
 @if($data['name'] != '')
-<span style="font-family: arial; font-size: 12px;">
+<span style="font-family: arial; font-size: 12px;padding-bottom:5px;">
  Nombre: <strong>{{$data['name']}}</strong>	
 </span>
 <br>
 @endif
 
 @if($data['email'] != '')
-<span style="font-family: arial; font-size: 12px;">
+<span style="font-family: arial; font-size: 12px;padding-bottom:5px;">
  Email: <strong>{{$data['email']}}</strong>	
 </span>
 <br>
 @endif
 
 @if($data['celular'] != '')
-<span style="font-family: arial; font-size: 12px;">
+<span style="font-family: arial; font-size: 12px;padding-bottom:5px;">
  Celular: <strong>{{$data['celular']}}</strong>	
 </span>
 <br>
 @endif
 
 @if($data['departamento'] != '')
-<span style="font-family: arial; font-size: 12px;">
+<span style="font-family: arial; font-size: 12px;padding-bottom:5px;">
  Departamento: <strong>{{$data['departamento']}}</strong>	
 </span>
 <br>
@@ -40,17 +42,22 @@ Solicitud de contacto por web:
 @endif
 
 @if($data['problema'] != '')
-<span style="font-family: arial; font-size: 12px;">
+<span style="font-family: arial; font-size: 12px;padding-bottom:5px;">
  Problema: <strong>{{$data['problema']}}</strong>	
 </span>
 <br>
 @endif
 
 
-
-<div style="padding: 10px; border-radius: 4px; background-color: #f8f8f8;" >
- <strong>{{$data['problema']}}</strong> 
-</div>
-
+@if($data['mensaje'] != '')
 <br>
+<span style="font-family: arial; font-size: 12px;padding-bottom:5px;">
+ Mensaje:  <strong>{{$data['mensaje']}}</strong> 
+</span>
+<br>
+@endif
+
+
+
+
 
