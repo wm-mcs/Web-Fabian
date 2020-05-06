6f568a0b66c4f9390a3a3f206bd28af501fef172
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-06 20:14:28 -0300

    df

--- app/Managers/envio_contacto_manager.php
+++ app/Managers/envio_contacto_manager.php
@@ -14,7 +14,7 @@ class envio_contacto_manager extends ManagerBase
   public function getRules()
   {
     $rules = [
-      'name'      => 'required|alpha',
+      'name'      => 'required',
       'email'     => 'required|email',
       'mensaje'   => 'required'
              ];
